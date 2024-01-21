6c2b52996aba3bb1d12930c0f3900de9fc57104b
hmh <hmh290204@gmail.com>
2024-01-21 07:27:34 +0700

    fix:

--- stock/config.py
+++ stock/config.py
@@ -1,9 +1,6 @@
 from ssi_fc_data import fc_md_client , model
 import pandas as pd 
 
-
-
-## Get token
 auth_type = 'Bearer'
 consumerID = 'e236036eebb74c79a956e577eefae726'
 consumerSecret = 'a6704fef62be44ff8aac9358a4a87805'
