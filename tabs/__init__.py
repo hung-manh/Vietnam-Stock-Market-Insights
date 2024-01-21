aa323093b2966b373cf9822df3540325d0c6ac3d
hmh <hmh290204@gmail.com>
2024-01-21 08:05:29 +0700

    feat:

--- tabs/__init__.py
+++ tabs/__init__.py
@@ -2,3 +2,4 @@ from .config import *
 from .gioi_thieu  import *
 from .danh_sach_niem_yet import *
 from .phan_tich_co_ban import *
+from .phan_tich_ky_thuat import *
---
+++ tabs/phan_tich_ky_thuat.py
@@ -0,0 +1,21 @@
+from .config import *
+
+# Ở cái tab này thì tôi muốn hiển thị ra một cái bảng nến 
+# Tôi viết ở stock file chart nhưng đang bị lỗi  
+
+
+# cái tab này sẽ có các lựa chọn như sau
+# - Chọn mã cổ phiếu 
+# - Chọn thời gian
+# - và cái biểu đồ nến thể hiện giá cổ phiếu theo thời gian với trục x là trục thời gian và trục y là giá cổ phiếu
+# - và cái biểu đồ đường thể hiện giá đóng cửa của cổ phiếu 
+
+def candlestick_chart(df):
+    pass
+
+def line_chart(df): 
+    pass    
+
+def render():
+    st.write('Phân tích kỹ thuật')
+    st.write('Đang cập nhật')
